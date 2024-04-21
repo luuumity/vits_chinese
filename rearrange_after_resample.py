f01bad190224c0e3b534d1cee3be3c4896d70550
ted stinson <30837044+luuumity@users.noreply.github.com>
2024-04-21 20:17:17 +0800

    Update rearrange_after_resample.py

--- rearrange_after_resample.py
+++ rearrange_after_resample.py
@@ -4,12 +4,12 @@ import re
 
 folder_path = "./vits_data/waves-16k/auxiliary"
 
-# 获取文件夹中所有文件的列表
+# 仅获取auxiliary文件的列表（仅处理auxiliary数据）
 file_list = os.listdir(folder_path)
 # 需要按字母顺序排列一下，以免最后文本和音频对不上！
 file_list = sorted(file_list)
 
-print(f"重采样后共有{len(file_list)}条音频。")
+print(f"auxiliary data 重采样后共有{len(file_list)}条音频。")
 
 # 重新整理文本，从原文本中选取与选用的辅助音频对应的文本
 content = "./input.txt"
@@ -19,16 +19,18 @@ L = len(file_list)
 with open(content, 'r') as f:
     line = f.readline()
     while line and i < L:
-        matches = re.match(r"(auxiliary[0-9]+.wav)(.*)", line)
-        if matches:
-            name = matches.group(1)
-            # print(name)
-            # print(file_list[i])
+        match1 = re.match(r"(SJY[0-9]+.wav)(.*)", line)
+        match2 = re.match(r"(auxiliary[0-9]+.wav)(.*)", line)
+        if match1:
+            # 如果是我的数据，直接保留。
+            inputtxt.append(line)
+        if match2:
+            # 如果是辅助数据，那些大于3s的，没被跳过的才保留。
+            name = match2.group(1)
             if name == file_list[i]:
                 inputtxt.append(line)
                 i += 1
-        else:
-            print("正则匹配出现问题。")
+
         line = f.readline()
 
 # 重写input文件
